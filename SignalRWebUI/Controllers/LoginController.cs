f5bb283d96faf6abafd1a1f6b92cf1aa06f0686a
emrekaraomeroglu@gmail.com <emrekaraomeroglu@gmail.com>
2024-01-30 00:34:09 +0300

    authorization işlemleri

--- SignalRWebUI/Controllers/LoginController.cs
+++ SignalRWebUI/Controllers/LoginController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SignalR.EntitiyLayer.Entities;
 using SignalRWebUI.Dtos.LoginDtos;
 
 namespace SignalRWebUI.Controllers
 {
+	[AllowAnonymous]
 	public class LoginController : Controller
 	{
 		private readonly SignInManager<AppUser>_signInManager;
--- SignalRWebUI/Program.cs
+++ SignalRWebUI/Program.cs
@@ -1,14 +1,26 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using SignalR.DataAccessLayer.Concrete;
 using SignalR.EntitiyLayer.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var requireAutorizePolicy=new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
 // Add services to the container.
 builder.Services.AddDbContext<SignalRContect>();
 builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<SignalRContect>();
-builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient();
+//authorize iþlemini kontrol etmek için 
+builder.Services.AddControllersWithViews(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter(requireAutorizePolicy));
+});
 
+//path vermek için
+builder.Services.ConfigureApplicationCookie(opts =>
+{
+    opts.LoginPath = "/Login/Index";
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,7 +35,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+//açýlýþ sayvasý için eklendi 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
